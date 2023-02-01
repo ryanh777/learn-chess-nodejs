96bf9ea79737a825ddddce746c062738af2b2337
Ryan Humphrey <ryan@Ryans-MacBook-Pro.local>
2023-01-31 23:20:20 -0500

    added port from env; app.env in gitignore

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ const Student = require("./models/Student")
 require("dotenv/config");
 
 const app = express();
-// const PORT = process.env.PORT || 5000;
-const PORT = 5000;
+const PORT = process.env.PORT || 808;
 
 // Data parsing
 app.use(express.json());
