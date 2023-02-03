c3af40e561447a0207fe82fad6c06ee596634ba1
Ryan Humphrey <ryanhumphrey777@gmail.com>
2023-02-03 00:20:14 -0500

    bcrypt, jwt

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const Student = require("./models/Student")
 require("dotenv/config");
 
 const app = express();
@@ -16,29 +15,8 @@ app.use(
 );
 
 // Routes
-// app.use("/user", require("./routes/userRoutes"));
-// app.use("/data", require("./routes/dataStore"));
-
-app.post("/", async (req, res) => {
-    // try {
-    //     console.log(req.body)
-    //     res.json("gucci in the coochi")
-    // } catch (err) {
-    //     console.log('error')
-    //     res.json(err)
-    // }
-    const student = new Student({
-        name: req.body.name,
-        grade: req.body.grade
-     });
-  
-     try {
-        const savedStudent = await student.save();
-        res.json(savedStudent._id);
-     } catch (err) {
-        res.json(err);
-     }
-})
+app.use("/user", require("./routes/userRoutes"));
+app.use("/data", require("./routes/dataStore"));
 
 // Connect to database
 mongoose.set('strictQuery', true);
